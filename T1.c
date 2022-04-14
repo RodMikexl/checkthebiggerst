4fae1bfef65c01347864b8553205a4d9a5a0cf07
Weihong <a1071108631@gmail.com>
2022-04-14 16:58:28 +1000

    C file

---
+++ T1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void lagrestOfThree(int x, int y, int z){
+
+}
+
+//when the user enter the 5 value, it will show three of the biggest one
+int main(){
+    
+    
+}
+
