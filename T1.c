71fcefeeebb7c1ee85f21325ceb2e5a8d31e8e15
Weihong <a1071108631@gmail.com>
2022-04-15 19:19:47 +1000

    largest function

--- T1.c
+++ T1.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//check the largest function
 void lagrestOfThree(int x, int y, int z){
-
+    if(x>=y && x>=z ){
+        printf("largest of three numbers: %d",x);
+    }
+    else if(y>=x && y>=z ){
+        printf("largest of three numbers: %d",y);
+    }
+    else{
+        printf("largest of three numbers: %d",z);
+    }
 }
 
 //when the user enter the 5 value, it will show three of the biggest one
